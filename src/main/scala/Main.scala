d44cf957e3d3a2f71cb8afa5a8a9fa24428d655e
ShalArl <a.rlind_shala@yahoo.com>
2022-04-01 11:04:07 +0200

    create Cards

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,2 +1,18 @@
-@main def run =
-  println("Poker")
+@main def hello: Unit = {
+    val card1 = new Card("A","Heart")
+    val card2 = new Card("K","Heart")
+    val card3 = new Card("Q","Heart")
+    val card4 = new Card("J","Heart")
+    val card5 = new Card("10","Heart")
+
+    println("Hello world!")
+    println(msg)
+}
+
+case class Card(picture:String, symbol:String) {
+    override def toString = picture + "("+symbol+")"
+}
+
+def msg = "I was compiled by Scala 3. :)"
+
+

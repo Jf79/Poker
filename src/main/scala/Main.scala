d9fd8f1178f43ff014b0ba448c5f7bb370f95dbe
Jf79 <jf.mayer@ymail.com>
2022-04-01 11:21:59 +0200

    commit -m "Welcome text"

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,19 @@
 @main def run: Unit = {
-   val cards = createCards
-   cards.foreach(c => println(c.toString))
-} 
-
-def createCards:Array[Card] = {
-    val cards = new Array[Card](5)
-    cards(0) = new Card("A","Heart")
-    cards(1) = new Card("K","Heart")
-    cards(2) = new Card("Q","Heart")
-    cards(3) = new Card("J","Heart")
-    cards(4) = new Card("10","Heart")
-    cards
+  println("Welcome to Poker")
+  val cards = createCards
+  cards.foreach(c => println(c.toString))
 }
 
-case class Card(picture:String, symbol:String) {
-    override def toString = picture + "("+symbol+")"
+def createCards: Array[Card] = {
+  val cards = new Array[Card](5)
+  cards(0) = new Card("A", "Heart")
+  cards(1) = new Card("K", "Heart")
+  cards(2) = new Card("Q", "Heart")
+  cards(3) = new Card("J", "Heart")
+  cards(4) = new Card("10", "Heart")
+  cards
 }
 
+case class Card(picture: String, symbol: String) {
+  override def toString = picture + "(" + symbol + ")"
+}

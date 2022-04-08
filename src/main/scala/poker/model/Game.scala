e74757b854877ee3e8694f2ba9d30b324ce1ce49
ShalArl <a.rlind_shala@yahoo.com>
2022-04-08 10:47:35 +0200

    methods added

--- src/main/scala/poker/model/Game.scala
+++ src/main/scala/poker/model/Game.scala
@@ -3,29 +3,32 @@ import scala.util.Random
 
 object Game {
 
-    private[Game] def getSymbols() : List[Symbol] = {
+    def getSymbols() : List[Symbol] = {
         val symbols = List(Symbol.HEART, Symbol.DIAMOND, Symbol.CLUB, Symbol.SPADE)
         symbols
     }
 
-    private[Game] def getPicutres() : List[Picture] = {
+    def getPicutres() : List[Picture] = {
         val pictures = List(Picture.TWO, Picture.THREE, Picture.FOUR, Picture.FIVE, Picture.SIX, Picture.SEVEN, 
         Picture.EIGHT, Picture.NINE, Picture.TEN, Picture.JACK, Picture.QUEEN, Picture.KING, Picture.ACE)
         pictures
     } 
 
-   /* def createCards() : Array[Card] = {
-        
-    }*/
 
-    /*+:* def getRandomCard(cards: Array[Card]) : Card = {
-        val random = new Random(cards.length)
-    }*/
+    def createCards() : Array[Card] = {
+       // Todo: create all 52 cards 
+       null 
+    }
 
-   /* def getRandomCards(cards: Array[Card], number: Int) : Array[Card] = {
-        val playerCards = new Array[Card](number)
-        
-    }*/
+    def getRandomCard(cards: Array[Card]) : Card = {
+        // Todo: get random card of the array cards
+        null
+    }
+
+    def getRandomCards(cards: Array[Card], n: Int) : Array[Card] = {
+        // Todo: get n random cards of the array cards, using the function getRandomCard
+        null
+    }
 
 }
 

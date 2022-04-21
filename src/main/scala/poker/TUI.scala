1254a997c4ac6f062297c21e85ff5f0e7639086f
ShalArl <a.rlind_shala@yahoo.com>
2022-04-21 15:36:17 +0200

    Klasse TUI erzeugt

---
+++ src/main/scala/poker/TUI.scala
@@ -0,0 +1,14 @@
+package poker
+import model._
+
+class TUI(player:Player) {
+  
+  /*  def run = {
+        player = new Player(new List[Card])
+    }
+
+    def createDeck: List[Card] =
+         Game.createCard()
+*/
+
+}

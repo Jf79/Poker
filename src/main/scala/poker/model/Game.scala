4f40a807615f2c37469ff01d083252023467b01e
ShalArl <a.rlind_shala@yahoo.com>
2022-04-21 15:36:46 +0200

    Methoden verbessert

--- src/main/scala/poker/model/Game.scala
+++ src/main/scala/poker/model/Game.scala
@@ -41,32 +41,23 @@ object Game {
     cards
   }
 
-  def getRandomCard(cards: Array[Card], randomIndex: Int): (Card, Array[Card]) = {
-    val indexCard = (math.random * (randomIndex - 1)).toInt
-    val card = cards(indexCard)
-    val deck = new Array[Card](cards.length - 1)
+  def getRandomCard(cards: Array[Card], rand: Int): (Card, Array[Card]) = {
+    val card = cards(rand)
     var index = 0
-    for (i <- 0 to cards.length - 1) {
-      if (i != indexCard) {
-        deck(index) = cards(i)
-        index += 1
-      }
-    }
-    (card, deck)
+    val newdeck = cards.filter(!_.equals(card))
+    (card, newdeck)
   }
 
-  def getRandomCards(cards: Array[Card],n: Int, randomIndex: Int): (Array[Card], Array[Card]) = {
-    // Todo: get n random cards of the array cards, using the function getRandomCard(cards)
-    val hand = new Array[Card](5)
+  def getRandomCards(cards: Array[Card], n: Int): (Array[Card], Array[Card]) = {
+    val randCards = new Array[Card](n)
     var deck = cards
-    var rI = randomIndex
     for (i <- 0 to n - 1) {
-      val tuple = getRandomCard(deck, rI)
-      hand(i) = tuple._1
+      val rand = Random.nextInt(deck.length - 1)
+      val tuple = getRandomCard(deck, rand)
+      randCards(i) = tuple._1
       deck = tuple._2
-      rI -= 1
     }
-    (hand, deck)
+    (randCards, deck)
   }
 
 }

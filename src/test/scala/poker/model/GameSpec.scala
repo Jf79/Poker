8ee44de00081bb89924ac4a412059ae2651f886e
ShalArl <a.rlind_shala@yahoo.com>
2022-04-21 12:47:37 +0200

    Tests hinzugefügt

--- src/test/scala/poker/model/GameSpec.scala
+++ src/test/scala/poker/model/GameSpec.scala
@@ -7,5 +7,22 @@ import model._
 
 
 class GameSpec extends AnyWordSpec with Matchers { 
-    
+    "The Symbols" when {
+        "there created" should {
+            val symbols = List(Symbol.HEART, Symbol.DIAMOND, Symbol.CLUB, Symbol.SPADE)
+            "be (HEART, DIAMOND, CLUB, SPADE)" in {
+                symbols should be(Game.getSymbols())
+            }
+        }
+    }
+    "The Pictures" when {
+        "there created" should {
+            val pictures = List(Picture.TWO,Picture.THREE,Picture.FOUR,Picture.FIVE,Picture.SIX,Picture.SEVEN,
+            Picture.EIGHT,Picture.NINE,Picture.TEN,Picture.JACK,Picture.QUEEN, Picture.KING,
+            Picture.ACE)
+            "be (TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE)" in {
+                pictures should be(Game.getPicutres())
+            }
+        }
+    }
 }

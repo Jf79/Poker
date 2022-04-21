19e4d0bb3fe8790623eaebab190626753871cf4d
ShalArl <a.rlind_shala@yahoo.com>
2022-04-21 23:24:24 +0200

    Methoden erweitert

--- src/main/scala/poker/view/TUI.scala
+++ src/main/scala/poker/view/TUI.scala
@@ -11,52 +11,55 @@ class TUI() {
       println("Good Luck !")
       val money = 500
       var player = new Player(new Array[Card](5), money)
-      gameLoop(money, player)
+      gameLoop(player)
       println("Your remaining money: " + player.money)
     }
 
-    def gameLoop(money: Int, player:Player) : Player = {
+    def gameLoop(player:Player) : Player = {
       var continue = true
-      var theMoney = money
       var thePlayer = player
       while(continue) {
-        val deck = createDeck()
+        val deck = createDeck() 
         val randCardsAndDeck = getRandomCards(deck, 5)
         thePlayer = new Player(randCardsAndDeck._1, thePlayer.money)
-        println(printGame(player))
-        thePlayer = startRound(player, randCardsAndDeck._2)
-        println(printGame(player))
-        continue = checkInput(readLine("Do you wanna continue(c) or quit(q) ?"))
+        println(printGame(thePlayer))
+        thePlayer = startRound(thePlayer, randCardsAndDeck._2)
+        println(printGame(thePlayer))
+        continue = checkInput(readLine("Do you wanna continue(c) or quit(q) ?\n"))
       }
       thePlayer
     }    
 
     def startRound(player: Player, deck: Array[Card]): Player =
-      val newCards = checkInput(readLine("Which cards you wanna hold ?").split(" "), deck)
+      val newCards = checkInput(readLine("Which cards you wanna hold ?\n").split(" "), deck, player)
       val money = player.money + 0
       new Player(newCards, money)
 
-    def checkInput(input: Array[String], deck: Array[Card]): Array[Card] = 
+    def checkInput(input: Array[String], deck: Array[Card], player: Player): Array[Card] = 
       val cards = new Array[Int](input.length)
       var i = 0
       for(c <- input)
         cards(i) = c.toInt
-      holdCards(deck, cards)
+        i += 1
+      holdCards(deck, cards, player)
     
-    def holdCards(deck:Array[Card], holdedCards:Array[Int]): Array[Card] = 
-      val hand = new Array[Card](5)
-      hand
+    def holdCards(deck:Array[Card], holdedCards:Array[Int], player: Player): Array[Card] = 
+      val newHand = Game.getRandomCards(deck, 5)._1
+      val oldHand = player.cards
+      for(h <- holdedCards)
+        newHand(h - 1) = oldHand(h - 1)
+      newHand
 
     def checkInput(input: String): Boolean =
       input.equals("c")
 
     def printGame(player: Player): String = 
       var game = "\n"
-      game += printNumbers() + printCards(player.cards)
+      game += printNumbers() + printCards(player.cards) + "\n\n"
       game
 
     def printCards(hand: Array[Card]): String =
-      var cards = "\n"
+      var cards = "\n\n"
       hand.foreach(h => cards += h.toString + "\t")
       cards
 

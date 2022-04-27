1d058390f78be9b7134960edd0009d978820ce0f
ShalArl <a.rlind_shala@yahoo.com>
2022-04-27 23:52:37 +0200

    verbessert

--- src/main/scala/poker/Poker.scala
+++ src/main/scala/poker/Poker.scala
@@ -1,14 +1,15 @@
 package poker
 import model._
 import view._
+import poker.controller.Controller
+import java.util.ResourceBundle.Control
 
-object start {
+object start :
 
-    @main def run: Unit = {
-    println("\nWelcome to Poker")
-    val tui = new TUI()
-    tui.run
-    println("\nGoodbye")
-  }
-}
+    @main def run: Unit = 
+      println("\nWelcome to Poker")
+      val controller = new Controller()
+      val tui = new TUI(controller)
+      tui.run
+  
 

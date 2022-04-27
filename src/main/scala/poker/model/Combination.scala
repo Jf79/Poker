66e92b7d90f45228418a2e830af58bed280d574b
ShalArl <a.rlind_shala@yahoo.com>
2022-04-27 13:20:14 +0200

    Enum für Kombinationen

--- src/main/scala/poker/model/Combination.scala
+++ src/main/scala/poker/model/Combination.scala
@@ -7,4 +7,6 @@ enum Combination(multFactor: Int):
     case STRAIGHT  extends Combination(4)
     case FLUSH extends Combination(6)
     case FULL_HOUSE extends Combination(9)
-    case FOUR_OF_A_KIND extends Combination(10)
+    case FOUR_OF_A_KIND extends Combination(25)
+    case STRAIGHT_FLUSH extends Combination(50)
+    case ROYAL_FLUSH extends Combination(250)

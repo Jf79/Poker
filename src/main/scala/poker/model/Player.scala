2a1a79e7b76bc6bb4d7e6e22b4ff93711a042e94
ShalArl <a.rlind_shala@yahoo.com>
2022-04-27 23:53:42 +0200

    verbessert

--- src/main/scala/poker/model/Player.scala
+++ src/main/scala/poker/model/Player.scala
@@ -2,7 +2,10 @@ package poker.model
 import model._
 
 case class Player (hand: Array[Card], money: Int) {
-    def this(money:Int) = this(new Array[Card](5), money)
+    
+    def this(money:Int) = this(null, money)
+    
     def setHand(hand: Array[Card]):Player = new Player(hand, this.money)
-    override def toString = "Money: " + money
+    
+    override def toString = "Remaining money: " + money
 }

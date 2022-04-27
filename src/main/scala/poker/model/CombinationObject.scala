dd9213388ad1df3750c0dbe6c63fadb3b51fc332
ShalArl <a.rlind_shala@yahoo.com>
2022-04-27 13:19:55 +0200

    Object für die Kombinationen

---
+++ src/main/scala/poker/model/CombinationObject.scala
@@ -0,0 +1,32 @@
+package poker.model
+import Combination._
+
+object CombinationObject {
+
+    def findCombination(hand: Array[Card]): Combination =
+        null
+
+    def hasPair(hand: Array[Card]): (Boolean, Array[Card]) =
+        null
+    
+    def hasTwoPair(hand: Array[Card]): (Boolean, Array[Card]) =
+        null
+
+    def hasThreeOfAKind(hand: Array[Card]): (Boolean, Array[Card]) =
+        null
+    
+    def hasStraight(hand: Array[Card]): Boolean =
+        false
+    
+    def hasFullHouse(hand: Array[Card]): Boolean =
+        false
+
+    def hasFourOfAKind(hand: Array[Card]): (Boolean, Card) =
+        null
+    
+    def hasStraigthFlush(hand: Array[Card]): Boolean =
+        false
+
+    def hasRoyalFlush(hand : Array[Card]): Boolean = 
+        false
+}

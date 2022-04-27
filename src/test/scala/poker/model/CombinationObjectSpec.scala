8aa3c7cbe3343c0d757ee273673f170c4fe62551
ShalArl <a.rlind_shala@yahoo.com>
2022-04-27 13:22:13 +0200

    Testklasse für CombinationObject

---
+++ src/test/scala/poker/model/CombinationObjectSpec.scala
@@ -0,0 +1,9 @@
+package poker.model
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+
+class CombinationObjectSpec extends AnyWordSpec with Matchers {
+    
+}

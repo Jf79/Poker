824ece4bf4fa90b5e3c8fd572a102a6075a5b738
ShalArl <a.rlind_shala@yahoo.com>
2022-04-27 23:53:28 +0200

    mvc

---
+++ src/main/scala/poker/controller/Controller.scala
@@ -0,0 +1,29 @@
+package poker
+package controller
+
+import util.Observable
+import model.Round
+import model.Card
+
+case class Controller(var round: Round) extends Observable {
+    
+    def this() = this(null)
+
+    def setRound(round: Round) = 
+        this.round = round
+        notifyObservers
+
+    def setPlayerCards() = 
+        round = round.setPlayerCards()
+        notifyObservers
+    
+    def holdCards(holdedCards: Vector[Int]) = 
+        round = round.holdCards(holdedCards)
+        notifyObservers
+    
+    override def toString = 
+        round.toString
+
+}
+    
+

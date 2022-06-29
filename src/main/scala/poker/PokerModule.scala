759a1864d7ac1a565867e790a2ff2c2f9afd2e01
Jf79 <jf.mayer@ymail.com>
2022-06-29 12:23:02 +0200

    xml und json

--- src/main/scala/poker/PokerModule.scala
+++ src/main/scala/poker/PokerModule.scala
@@ -5,7 +5,7 @@ import controller.controllerBaseImpl.Controller
 import controller.ControllerInterface
 import model.player.PlayerInterface
 import model.player.playerBaseImpl.Player
-import model.fileIO.fileIOjson.FileIO
+import model.fileIO.fileIOxml.FileIO
 import model.card.CardInterface
 import aview.gui.GUI
 import aview.tui.TUI
@@ -16,6 +16,6 @@ class PokerModule extends AbstractModule:
     override def configure(): Unit = 
         bind(classOf[PlayerInterface]).toInstance(new Player(200))
         bind(classOf[ControllerInterface]).toInstance(new Controller(new Player(200), new FileIO()))
-        bind(classOf[UserInterface]).toInstance(new TUI(new Controller(new Player(200), new FileIO())))
+        bind(classOf[UserInterface]).toInstance(new GUI(new Controller(new Player(200), new FileIO())))
 
 
